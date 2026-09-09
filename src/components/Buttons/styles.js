5542995f7f89924ec1931cee67ae831f3c066495
MarcelDBarbosa <marcdupr@gmail.com>
2026-09-09 18:21:16 -0300

    commit inicial

---
+++ src/components/Buttons/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components"; 
+
+export const ButtonContainer = styled.button`
+    width = 100px;
+    height = 100px;
+    border: 1px solid #CDCDCD;
+    font-size: 24px;
+    font-weight: 500;
+    flex: 1;
+
+    &:hover{
+        opacity: 0.6;
+    }
+`
